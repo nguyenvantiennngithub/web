f0946a7440b10e48e7d375345a92986c7c6142ba
VanTiennn <nguyenvantiennn0910@gmail.com>
2020-09-23 18:38:39 +0700

    testtt

--- src/index.js
+++ src/index.js
@@ -6,21 +6,16 @@ const { UV_FS_O_FILEMAP } = require('constants');
 const router = require('./router/index.router');
 const app = express();
 
-    app.use(morgan('combined'));
+app.use(morgan('combined'));
 
-    app.engine('.hbs', exphbs({ extname: '.hbs' }));
-            app.set('view engine', '.hbs');
+app.engine('.hbs', exphbs({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
 
-
-
-
-
-            
-    app.use(express.static(path.join(__dirname, 'public')));
-    app.set("views", path.join(__dirname, 'resources', 'views'));
+            app.use(express.static(path.join(__dirname, 'public')));
+            app.set('views', path.join(__dirname, 'resources', 'views'));
             app.use(express.urlencoded({ extended: true }));
             app.use(express.json());
 
-    router(app);
+router(app);
 
-    app.listen(8080);
+app.listen(8080);
