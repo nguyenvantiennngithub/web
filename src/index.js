edadc8939b60ed8d256568ba997151e42ef4390b
VanTiennn <nguyenvantiennn0910@gmail.com>
2020-09-23 20:05:48 +0700

    test

--- src/index.js
+++ src/index.js
@@ -11,10 +11,10 @@
 app.engine('.hbs', exphbs({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
-                    app.use(express.static(path.join(__dirname, 'public')));
-                                    app.set('views', path.join(__dirname, 'resources', 'views'));
-                                    app.use(express.urlencoded({ extended: true }));
-                                    app.use(express.json());
+                                app.use(express.static(path.join(__dirname, 'public')));
+                                                app.set('views', path.join(__dirname, 'resources', 'views'));
+                                                app.use(express.urlencoded({ extended: true }));
+                                                app.use(express.json());
 
 router(app);
 
